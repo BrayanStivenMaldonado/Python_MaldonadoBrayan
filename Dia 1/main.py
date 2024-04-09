d9c7d9f7ecdf3eca47560e4ea2c4a24eb12a22f9
BrayanStivenMaldonado <stivenmaldonado0805@gmail.com>
2024-04-09 12:50:26 -0500

    Conversion y tipos ciclo while hecho

--- Dia 1/main.py
+++ Dia 1/main.py
@@ -1,6 +1,54 @@
 #--------------------------------#
 #----DIA 1 CHEAT SHEET PYTHON----#
 #--------------------------------#
+
 #imprimir hola mundo
 print("Hola mundoooooo!")
+
+#Datos primitivos 
+
+#Numero
+Numerito = 1 #Nombre variable = valor
+print(Numerito) #Imprimir (variable)
+print(type(Numerito)) #Imprimir (tipo(variable))
+
+#Decimal
+NumeroDecimal = 1.1
+print(NumeroDecimal)
+print(type(NumeroDecimal))
+
+#Booleano
+MiBooleanito = True
+print(MiBooleanito)
+print(type(MiBooleanito))
+
+#CadenaDeTexto
+Texto = "Queloque"
+print(Texto)
+print(type(Texto))
+ 
+ #TALLER
+ 
+#Cómo se ingresa información por teclado
+Informacion = input("Ingrese un dato ")
+print("El dato ingresado es:",Informacion)
+
+#Conversion de tipos de variable (tipos de datos primitivos)
+print(int(2))
+print(float(2))
+print(str("Dos"))
+print(bool(True))
+print(bool(False))
+
+#Bucles For y While
+#For
+
+#While
+numero = 1
+while numero <= 10:
+    print(numero)
+    numero +=1
+print("Proceso terminado")
+#Funciones (4 tipos)
+
 #Desarrollado por Brayan Maldonado - Camper - TI 1.090.404.470
