c085639672bfccd042f268854e45404c7631829e
BrayanStivenMaldonado <stivenmaldonado0805@gmail.com>
2024-04-09 11:47:24 -0500

    Primer commit

--- Dia 1/main.py
+++ Dia 1/main.py
@@ -1,7 +1,6 @@
 #--------------------------------#
 #----DIA 1 CHEAT SHEET PYTHON----#
 #--------------------------------#
-
 #imprimir hola mundo
 print("Hola mundoooooo!")
 #Desarrollado por Brayan Maldonado - Camper - TI 1.090.404.470
