b38a551ba8b8ed67b38ee48ace8507136edb2a0c
BrayanStivenMaldonado <stivenmaldonado0805@gmail.com>
2024-04-09 11:47:14 -0500

    Carpeta Python

---
+++ main.py
@@ -0,0 +1,7 @@
+#--------------------------------#
+#----DIA 1 CHEAT SHEET PYTHON----#
+#--------------------------------#
+
+#imprimir hola mundo
+print("Hola mundoooooo!")
+#Desarrollado por Brayan Maldonado - Camper - TI 1.090.404.470
