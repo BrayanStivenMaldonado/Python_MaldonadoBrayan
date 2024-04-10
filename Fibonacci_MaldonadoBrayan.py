81f2d9974c914feabfcfcf50033f4a66d2dcf3bb
BrayanStivenMaldonado <stivenmaldonado0805@gmail.com>
2024-04-10 12:58:18 -0500

    Actividad en clase

---
+++ Fibonacci_MaldonadoBrayan.py
@@ -0,0 +1,5 @@
+print("Estructura Fibonacci")
+print("Se trata de una secuencia infinita de números naturales; a partir del 0 y el 1, se van sumando a pares, de manera que cada número es igual a la suma de sus dos anteriores,")
+int=n = input (int("Ingrese un valor entero (n) para saber hasta qué número se mostrará su estructura"))
+for i in range(n):
+    print[i]
