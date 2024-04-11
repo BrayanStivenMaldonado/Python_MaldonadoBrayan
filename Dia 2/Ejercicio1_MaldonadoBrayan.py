21ae5c97f4c57badc91b09081318547c69b76400
BrayanStivenMaldonado <stivenmaldonado0805@gmail.com>
2024-04-11 11:16:30 -0500

    terminado

--- Dia 2/Ejercicio1_MaldonadoBrayan.py
+++ Dia 2/Ejercicio1_MaldonadoBrayan.py
@@ -1,31 +1,31 @@
 #Estructura de Fibonacci
 
+#se le explica al usuario el cómo funciona la estructura de Fibonacci
 print("Estructura Fibonacci")
 print("Se trata de una secuencia infinita de números naturales; a partir del 0 y el 1, se van sumando a pares, de manera que cada número es igual a la suma de sus dos anteriores,")
-while True:
+while True: #mientras sea verdadero, repetir
 
-    cantidad =int(input("Ingrese un valor entero para saber hasta qué número se mostrará su estructura "))
+    cantidad =int(input("Ingrese un valor entero para saber hasta qué número se mostrará su estructura ")) #se le genera la entrada para que ingrese la cantidad de número que desea mostrar en pantalla
     a = 0
     b = 1
     print(" ")
-    for i in range(1,cantidad) :
-        while a < 89:
-            print(a)
+    for i in range(1,cantidad) : #para i en rango (desde 1 a cantidad), repetir 
+        while a < 89: #el valor del número 11 es 55, entonces se dice que se corten los números antes de que llegue al valor de 12, que es 89
+            print(a) #Se muestra el valor de a
             break
-        c = a + b
-        a = b
-        b = c
+        c = a + b #c es la suma de los valores de a y b
+        a = b #a pasa a ser el valor de a
+        b = c #b pasa a ser el valor de c
 
-    r=int(input("""
-    1). Repetir el programa
+    r=int(input(""" 
+    1). Repetir el programa  
     2). Cerrar el programa
-    """))   
+    """))  #se le pregunta al usuario si desea volver a usar el programa, y se le genera la respectiva entrada para que ingrese la respuesta
 
-    if r==1:
+    if r==1: #si escoge 1, el ciclo se va a volver a repetir
         print("BIENVENIDO DE NUEVO AL PROGRAMA")
 
-    elif r==2:
+    elif r==2: #si escoge el 2, se cierra el programa
         print("Nos vemos luego c:")
         break
-
 #Desarrollado por Brayan Maldonado - Camper - TI 1.090.404.470
--- Dia 2/Ejercicio2_MaldonadoBrayan.py
+++ Dia 2/Ejercicio2_MaldonadoBrayan.py
@@ -3,20 +3,21 @@
 print("   BIENVENIDO AL JUEGO DE ADIVINAR EL NUMERO ALEATORIO")
 print("""1). Tu objetivo va a ser adivinar el número en la menor cantidad de intentos
 2). Cada vez que hagas una suposición te voy a dar una pista
-""")
-import random
-aleatorio = random.randint(1,100)
-print(aleatorio)
-for i in range(1,100):
-    print("Ingrese un número del 1 al 100")
-    numero= int(input(""))
-    if numero==aleatorio:
-        print("c:")
-        break
-    if numero!=aleatorio:
-        if numero>aleatorio:
+""") #se le da la bienvenida al usuario y se le explica cómo funciona el juego
+import random #se importa la libreria "random"
+aleatorio = random.randint(1,100) #a la variable "aleatorio" se le da el valor de random.randint para que me de un valor entero al azar entre 1 y 100
+intentos = 0 #se le da un valor inicial a la variable "intentos" para usarlo como contador más adelante
+booleanito=True
+while booleanito==True: #mientras sea verdadero 
+    intentos += 1 #cada vez que se repita el ciclo, el valor de "intentos" 
+    print("Ingrese un número del 1 al 100, intento #",intentos)
+    numero= int(input("")) #se genera la entrada para que el usuario ingrese el valor que considere
+    if numero==aleatorio: #si el número ingresado por el usuario es igual al que generó el programa se le da el anuncio de que ganó
+        print("Has ganado") 
+        booleanito=False #y si se cumple esta condición, el programa se cierra
+    if numero!=aleatorio: #si el número ingresado por el usuario es diferente al generado por el programa se verifican las siguientes condiciones
+        if numero>aleatorio: #si el número ingresado es mayor al aleatorio se le da el anuncio de que debe ingresar uno más pequeño
             print("Pista: El número es más pequeño")
-        else:
-            print("Pista: El número es más grande")
-            
+        else: #si el número ingresado es menor al aleatorio se le da el anuncio de que debe ingresar uno más grande
+            print("Pista: El número es más grande")            
 #Desarrollado por Brayan Maldonado - Camper - TI 1.090.404.470
