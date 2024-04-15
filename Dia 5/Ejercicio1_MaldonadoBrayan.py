8cb897d3a2d2b92292e55141887bfb8b64f321ee
BrayanStivenMaldonado <stivenmaldonado0805@gmail.com>
2024-04-15 13:01:24 -0500

    Empezando:c

---
+++ Dia 5/Ejercicio1_MaldonadoBrayan.py
@@ -0,0 +1,8 @@
+CantNumeros = int(input("Ingrese la cantidad de valores que desea ingresar "))    
+n = []
+
+k = int(input("Ingrese el número por el que desea dividir las parejas que se creen"))
+
+for i in range (CantNumeros):
+    n.append(int(input("Ingrese sus valores ")))
+print(n)
